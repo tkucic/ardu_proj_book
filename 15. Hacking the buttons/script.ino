6f856c1bf57acad3baad0b439a4ba2c4bbebf21f
Toni Kucic <tkucic@gmail.com>
2020-12-20 19:42:04 +0200

    Add project 15

---
+++ 15. Hacking the buttons/script.ino
@@ -0,0 +1,11 @@
+const int optoPin = 2;
+
+void setup(){
+    pinMode(optoPin, OUTPUT);
+}
+void loop(){
+    digitalWrite(optoPin, HIGH);
+    delay(15);
+    digitalWrite(optoPin, LOW);
+    delay(1000);
+}
